cd59f96b9209c7f4a3e3d462d340a7384ecf0dc2
tsinghualiji <jbalticlee@gmail.com>
2014-10-20 14:58:44 +0800

    增加静态导入内容

---
+++ src/com/adobe/www/staticimport/StaticImport.java
@@ -0,0 +1,10 @@
+package com.adobe.www.staticimport;
+import static java.lang.Math.*;
+
+public class StaticImport {
+
+	public static void main(String[] args){
+
+		System.out.println(abs(-1));
+	}
+}
