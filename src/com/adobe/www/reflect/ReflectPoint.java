a4d9dbd94e87d9e7d76c9ae4760b14fe39ca7a05
tsinghualiji <jbalticlee@gmail.com>
2014-10-20 14:08:53 +0800

    增加反射内容

---
+++ src/com/adobe/www/reflect/ReflectPoint.java
@@ -0,0 +1,64 @@
+package com.adobe.www.reflect;
+
+public class ReflectPoint {
+
+
+	public ReflectPoint(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	private int x;
+	public int y;
+
+	public String star1 = "ball";
+	public String str2 = "basketball";
+	public String str3 = "scene777";
+
+	@Override
+	public String toString() {
+		return "ReflectPoint [star1=" + star1 + ", str2=" + str2 + ", str3="
+				+ str3 + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ReflectPoint other = (ReflectPoint) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+	
+}
---
+++ src/com/adobe/www/reflect/ReflectTest.java
@@ -0,0 +1,71 @@
+package com.adobe.www.reflect;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class ReflectTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		//获取Class文件的3种方法
+		String str1 = "abc";
+		Class cls1 = str1.getClass();
+		Class cls2 = String.class;
+		Class cls3 = Class.forName("java.lang.String");
+		System.out.println(cls1 == cls2);
+		System.out.println(cls3 == cls2);
+		System.out.println(cls1.isPrimitive());
+		System.out.println(int.class.isPrimitive());
+		
+		ReflectPoint pt1 = new ReflectPoint(3,5);
+		Field filedY = pt1.getClass().getField("y");
+		//fieldY的值是多少？filedY不是对象身上的变量，而是类上的，要用它去取某个对象的值。
+		System.out.println(filedY.get(pt1));
+		
+		Field filedX = pt1.getClass().getDeclaredField("x");
+		filedX.setAccessible(true);
+		System.out.println(filedX.get(pt1));
+		
+		changeStringValue(pt1);
+		System.out.println(pt1);
+		//先取出String这个类的class中的charAt方法的字节码，然后用这个字节码去作用在str1这个对象上去。
+		Method methodCharAt = String.class.getMethod("charAt", int.class);
+		//methodCharAt可以理解成一个方法对象。
+		System.out.println(methodCharAt.invoke(str1, 1));
+		//如果传递给Method对象的invoke方法的第一个参数为null,则说明该Method对象对应的是一个静态方法。
+		//TestArguments.main(new String[]{"123","345"});
+		String startClassName = args[0];
+		Method mainMethod = Class.forName(startClassName).getMethod("main", String[].class);
+		mainMethod.invoke(null, (Object)new String[]{"111","222","333"});
+	}
+
+	private static void changeStringValue(Object obj) throws Exception {
+		// TODO Auto-generated method stub
+		Field[] fields = obj.getClass().getFields();
+		for(Field field: fields){
+			//这里应该用==，不应该用equals,因为是同一份字节码
+			if(field.getType() == String.class){
+				String oldValue = (String)field.get(obj);
+				String newValue = oldValue.replace('b', 'a');
+				field.set(obj, newValue);
+			}
+		}
+	}
+}
+
+class TestArguments{
+	
+	public static void main(String args[]){
+		
+		for(String arg : args){
+			System.out.println(arg);
+		}
+	}
+}
+
+
+
+
