7e7b22b99b19f83672fffb48566e00a1e7b16dcb
tsinghualiji <jbalticlee@gmail.com>
2014-10-21 16:33:11 +0800

    增加内部类内容

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-###此工程作J2SE学习之用 点击[j2se](https://github.com/tsinghualiji/java/tree/master/src/com/adobe/www)
+####此工程作J2SE学习之用 点击[j2se](https://github.com/tsinghualiji/java/tree/master/src/com/adobe/www)
--- src/com/adobe/www/innerclass/InnerClassDemo.java
+++ src/com/adobe/www/innerclass/InnerClassDemo.java
@@ -1,28 +1,44 @@
-package com.adobe.www.innerclass;
+package innerclass;
 
-//非静态内部类
-class Outer{
-	public String name = "outer";
-	class Inner{
-		public String name = "inner";
-		public void test(){
+class Outer1
+{
+	private String name =  "outer";
+	class Inner//非静态内部类
+	{
+		private String name = "inner";
+		public void test()
+		{
 			String name = "test";
-			System.out.println("inner.test......");
+			System.out.println(name);//test
+			System.out.println(this.name);//inner
+			System.out.println(new Outer1().name);//outer
+			System.out.println(Outer1.this.name);//outer
 		}
 	}
-	public void show() {
+
+	public void show()
+	{	
+		//在外部类之内访问内部类
+		//this.test();//ERROR
+		//想执行内部类的test方法
 		new Inner().test();
-	}
+	}	
 }
 
-public class InnerClassDemo {
+public class InnerDemo
+{
+	public static void main(String[] args)
+	{
+		new Outer1().show();
+		//想在外部类之外来访问内部类,内部类不能私有
+		/*
+			非静态内部类访问:
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Outer outer = new Outer();
-		outer.new Inner().test();
-		new Outer().new Inner().test();
+				外部类.内部类  变量 = 外部类实例.new Inner();
+		*/
+		Outer1 out = new Outer1();//外部类对象
+		Outer1.Inner in = out.new Inner();//通过外部类对象,来创建内部类对象,因为 非静态内部类属于外部类的对象
+		in.test();
+		new Outer1().new Inner().test();
 	}
 }
